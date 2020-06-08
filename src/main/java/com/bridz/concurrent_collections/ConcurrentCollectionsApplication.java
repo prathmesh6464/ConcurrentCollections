19129f3715b5adbf8d592e4c733e403fc7d3af9e
prathamesh <prathmesh6464@gmail.com>
2020-06-08 16:26:17 +0530

    ADDED : Concurrent collection project.

---
+++ src/main/java/com/bridz/concurrent_collections/ConcurrentCollectionsApplication.java
@@ -0,0 +1,9 @@
+package com.bridz.concurrent_collections;
+
+public class ConcurrentCollectionsApplication {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/test/java/com/bridz/concurrent_collections/ConcurrentCollectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bridz.concurrent_collections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrentCollectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

ce4b6c9920210228ac87906f37459c5e2f47d394
prathamesh <prathmesh6464@gmail.com>
2020-06-08 20:41:16 +0530

    ADDED : Concurrent collections application.

--- src/main/java/com/bridz/concurrent_collections/ConcurrentCollectionsApplication.java
+++ src/main/java/com/bridz/concurrent_collections/ConcurrentCollectionsApplication.java
@@ -1,9 +1,38 @@
 package com.bridz.concurrent_collections;
 
-public class ConcurrentCollectionsApplication {
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Set;
 
-	public static void main(String[] args) {
+public class ConcurrentCollectionsApplication extends Thread {
 
+	static ConcurrentHashMap<Integer, String> concurrentHashMap = new ConcurrentHashMap();
+
+	public void run() {
+		try {
+			Thread.sleep(200);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		concurrentHashMap.put(1, "hii");
 	}
 
+	public static void main(String[] args) {
+		
+		concurrentHashMap.put(2, "how r u");
+		concurrentHashMap.put(3, "i am fine");
+
+		ConcurrentCollectionsApplication concurrentCollections = new ConcurrentCollectionsApplication();
+		concurrentCollections.start();
+
+		Set entrySet = concurrentHashMap.keySet();
+		entrySet.forEach(data -> {
+			System.out.println(concurrentHashMap.get(data));
+			try {
+				Thread.sleep(3000);
+			} catch (Exception exception) {
+				exception.printStackTrace();
+			}
+		});
+
+	}
 }

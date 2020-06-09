25e090ff9b142da3e3c793467e63e23cf390a27a
prathamesh <prathmesh6464@gmail.com>
2020-06-09 09:20:25 +0530

    REFACTORED : Added run method and added main method.

--- src/main/java/com/bridz/concurrent_collections/copy_on_arraylist/CopyOnWriteArrayListProgram.java
+++ src/main/java/com/bridz/concurrent_collections/copy_on_arraylist/CopyOnWriteArrayListProgram.java
@@ -1,5 +1,37 @@
 package com.bridz.concurrent_collections.copy_on_arraylist;
 
-public class CopyOnWriteArrayListProgram {
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class CopyOnWriteArrayListProgram extends Thread {
+
+	static CopyOnWriteArrayList<String> copyOnWriteArrayList = new CopyOnWriteArrayList();
+
+	public void run() {
+		try {
+			Thread.sleep(200);
+		} catch (Exception exception) {
+			exception.printStackTrace();
+		}
+		copyOnWriteArrayList.add("hii");
+	}
+
+	public static void main(String[] args) {
+
+		copyOnWriteArrayList.add("how r u");
+		copyOnWriteArrayList.add("i am fine");
+
+		CopyOnWriteArrayListProgram concurrentCollections = new CopyOnWriteArrayListProgram();
+		concurrentCollections.start();
+
+				copyOnWriteArrayList.forEach(data -> {
+					System.out.println(data);
+					try {
+						Thread.sleep(3000);
+					} catch (Exception exception) {
+						exception.printStackTrace();
+					}
+				});
+
+	}
 
 }
